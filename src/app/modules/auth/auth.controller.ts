32c7da85a2b9bfd24f36fa9b7e379d73ac4e667f
Nur Mohammad <mohammadnur5480@gmail.com>
2023-09-01 11:51:33 +0600

    added login user route

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -3,6 +3,7 @@ import catchAsync from '../../../shared/catchAsync';
 import sendResponse from '../../../shared/sendRespons';
 import httpStatus from 'http-status';
 import { AuthService } from './auth.service';
+import config from '../../../config';
 
 const insertIntoDb = catchAsync(async (req: Request, res: Response) => {
   const result = await AuthService.insertIntoDb(req.body);
@@ -13,5 +14,23 @@ const insertIntoDb = catchAsync(async (req: Request, res: Response) => {
     data: result,
   });
 });
+const loginUser = catchAsync(async (req: Request, res: Response) => {
+  const { ...loginData } = req.body;
+  const result = await AuthService.loginUser(loginData);
+  // set cookies
+  const { refreshToken, ...others } = result;
 
-export const AuthController = { insertIntoDb };
+  const cookieOptions = {
+    secure: config.env === 'production',
+    httpOnly: true,
+  };
+  res.cookie('refreshToken', refreshToken, cookieOptions);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'User logged in   successfully',
+    data: others,
+  });
+});
+
+export const AuthController = { insertIntoDb, loginUser };
--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,11 +1,62 @@
 import { User } from '@prisma/client';
 import prisma from '../../../shared/prisma';
+import config from '../../../config';
+import { Secret } from 'jsonwebtoken';
+import { jwtHelpers } from '../../../helper/jwtHelpers';
+import { ILoginUser } from './auth.interface';
+import ApiError from '../../../Errors/ApiError';
+import httpStatus from 'http-status';
+import bcrypt from 'bcrypt';
 
 const insertIntoDb = async (userData: User) => {
+  userData.password = await bcrypt.hash(
+    userData.password,
+    Number(config.bcrypt_salt_rounds)
+  );
   const result = await prisma.user.create({
     data: userData,
   });
   return result;
 };
 
-export const AuthService = { insertIntoDb };
+const loginUser = async (
+  payload: ILoginUser
+): Promise<{ accessToken: string; refreshToken: string }> => {
+  const { email, password } = payload;
+
+  const isUserExist = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (!isUserExist) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'User not found!');
+  }
+  const isMatchPass = await bcrypt.compare(password, isUserExist?.password);
+  if (isUserExist?.password && !isMatchPass) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Incorrect password!');
+  }
+
+  // access token and refresh token
+  const userEmail = isUserExist?.email;
+  const role = isUserExist?.role;
+  const accessToken = jwtHelpers.createToken(
+    { userEmail, role },
+    config.jwt.secret as Secret,
+    config.jwt.expires_in as string
+  );
+
+  const refreshToken = jwtHelpers.createToken(
+    { userEmail, role },
+    config.jwt.refresh_secret as Secret,
+    config.jwt.refresh_expires_in as string
+  );
+
+  return {
+    accessToken,
+    refreshToken,
+  };
+};
+
+export const AuthService = { insertIntoDb, loginUser };
